9f024a75455ffe23c6220a9f158fe5d0d9353cdf
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-11-15 21:33:34 +0100

    footer created/nav bar created with landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [			
+      AppComponent,
+      LandingPageComponent,
+      NavbarComponent,
+      FooterComponent
+   ],
   imports: [BrowserModule, MDBBootstrapModule.forRoot(), AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
