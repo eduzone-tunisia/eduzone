3411b8f3808e147f37c3c2cc377e98c7cbc8feb0
yasser massoud <yassermassoud000@gmail.com>
2020-11-18 16:01:26 +0100

    display data in home component suuccessfully

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,31 +7,18 @@ import {CousesService} from '../couses.service'
 })
 export class HomeComponent implements OnInit {
   courses: any;
-  frontend:any;
-  backend:any;
+  frontcourses: any;
+  backcourses: any;
   constructor(private coursesService: CousesService) { 
-    this.courses = this.coursesService.courses
+    this.frontcourses = this.coursesService.courses.filter(cate => cate.cate==="front-end")
+    this.backcourses = this.coursesService.courses.filter(cate => cate.cate==="back-end")
   }
-  slides: any = [[]];
-  chunk(arr: any[], chunkSize: number) {
-    let R = [];
-    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
-      if(arr[i].cate === "front-end"){
-      R.push(arr.slice(i, i + chunkSize));
-    }
-  }
-    return R;
-  }
+
   ngOnInit() {
-    this.slides = this.chunk(this.courses, 3);
+    
   }
 
 
-  filterCourse(cateid: any) {
-    const catecourses = this.courses.filter((course: { cate: any; }) => {
-      return course.cate === cateid;
-    });
-    return catecourses;
-  }
+ 
 
 }
