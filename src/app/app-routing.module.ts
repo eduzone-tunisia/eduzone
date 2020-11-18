fcda95e98cf665ccac37cb0f273407e0551bf4f6
Oussama <68253143+OussamaHeni@users.noreply.github.com>
2020-11-18 09:20:03 +0100

    commit to pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import{ RegisterComponent } from './register/register.component'
+import {LandingPageComponent} from './landing-page/landing-page.component'
 
 const routes: Routes = [
-  // {path:'', redirectTo: '/home', pathMatch: 'full' },
+   {path:'', component:LandingPageComponent },
   {path:'home',component:AppComponent},
   {path:'register',component:RegisterComponent},
 
