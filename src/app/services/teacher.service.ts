54a30e3bf3e905053f0e9640b4cdbb0f8a5c6e2a
Oussama <68253143+OussamaHeni@users.noreply.github.com>
2020-11-23 10:09:31 +0100

    finished toggling between profile photo in navbar and added avatar

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -22,7 +22,7 @@ export class TeacherService {
     return this.http.put(teacherUrl + `${id}`, teacherInfo);
   }
 
-  getConnectedTeacher(id : any) {
-    return this.http.get(teacherUrl + `${id}`)
+  getConnectedTeacher(id: any): Observable<any> {
+    return this.http.get(teacherUrl + id);
   }
 }
